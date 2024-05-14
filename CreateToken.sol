4c87e65d8ff2dc7706af3915a5970090dea8a517
rynbn <129083532+rynbn@users.noreply.github.com>
2024-05-14 19:17:38 +0800

    Update CreateToken.sol

--- CreateToken.sol
+++ CreateToken.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CreateToken is ERC20, Ownable {
+
+constructor() ERC20("CreateToken", "LIT") Ownable(msg.sender) {
+    // Mint initial tokens to the contract owner
+    _mint(msg.sender,  (uint256(decimals())));
+}
+
+    // Function to allow the owner to mint additional tokens
+    function mintTokens(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Function to allow anyone to burn their own tokens
+    function burnTokens(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens to burn");
+        _burn(msg.sender, amount);
+    }
+
+    // Function to transfer tokens to another address
+    function transferTokens(address to, uint256 amount) public {
+        require(to != address(0), "Invalid address");
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens to transfer");
+        _transfer(msg.sender, to, amount);
+    }
+}
